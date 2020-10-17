27de27849ca5909115af8bbd57bf2bcc982f9050
yoonjinlee <yoonjinifer@gmail.com>
2020-10-17 17:09:57 +0900

    update: 2주차 세미나 실습

---
+++ Seminar/week2/week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2
+//
+//  Created by 이윤진 on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
