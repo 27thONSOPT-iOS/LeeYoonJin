96f63e0b385d811568e31f5e28f7dd08f8a4938f
yoonjinlee <yoonjinifer@gmail.com>
2020-10-30 23:49:40 +0900

    update: 2주차 도전과제 제출

--- week2_HW/week2_HW/ViewController.swift
+++ week2_HW/week2_HW/ViewController.swift
@@ -8,19 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var listScrollView: UIScrollView!
+    @IBOutlet weak var moveTopBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setNaviBar()
-        // Do any additional setup after loading the view.
+        moveTopBtn.isHidden = true
+        listScrollView.delegate = self
+        moveTopBtn.layer.cornerRadius = moveTopBtn.frame.width/2
     }
+   
+    @IBAction func moveTop(_ sender: Any) {
 
+        listScrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+    }
+    
     func setNaviBar(){
 
         self.navigationController?.navigationBar.barTintColor = .black
         
-        
     }
 
+}
+
+extension ViewController: UIScrollViewDelegate{
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if listScrollView.contentOffset.y > 700{
+            print("state: \(listScrollView.contentOffset.y)")
+            moveTopBtn.isHidden = false
+        }else {
+            moveTopBtn.isHidden = true
+        }
+    }
 }
 
