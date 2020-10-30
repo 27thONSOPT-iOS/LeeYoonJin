b4101d7e326e60e4457c53cac5be85c59f2d79b3
yoonjinlee <yoonjinifer@gmail.com>
2020-10-30 18:37:32 +0900

    update: 2주차 과제 제출

---
+++ week2_HW/week2_HW/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  week2_HW
+//
+//  Created by 이윤진 on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNaviBar()
+        // Do any additional setup after loading the view.
+    }
+
+    func setNaviBar(){
+
+        self.navigationController?.navigationBar.barTintColor = .black
+        
+        
+    }
+
+}
+
